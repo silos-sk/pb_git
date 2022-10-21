b6865d9695c47315f9f1fd6e6f4bcd4bbe75f6c4
silos-sk <97029496+silos-sk@users.noreply.github.com>
2022-10-21 23:22:53 +0100

    portfolio nav js added

---
+++ portfolio/js/main.js
@@ -0,0 +1,15 @@
+const barIcon = document.querySelector("#bar-icon");
+const navBar = document.querySelector("nav");
+const xIcon = document.querySelector("#x-icon");
+
+barIcon.addEventListener("click", () => {
+  barIcon.classList.add("hide");
+  navBar.classList.add("active");
+  xIcon.classList.remove("hide");
+});
+
+xIcon.addEventListener("click", ()=>{
+    xIcon.classList.add("hide");
+    barIcon.classList.remove("hide")
+    navBar.classList.remove("active");
+});
