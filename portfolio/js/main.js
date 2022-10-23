c93f104fcd7ab38fea0f2b31b9fd78078e59c9d9
silos-sk <97029496+silos-sk@users.noreply.github.com>
2022-10-24 00:09:23 +0100

    added live portfolio items

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,6 +1,8 @@
 const barIcon = document.querySelector("#bar-icon");
 const navBar = document.querySelector("nav");
 const xIcon = document.querySelector("#x-icon");
+// const portfolioImg = document.querySelector(".portfolio-img");
+// const card = document.querySelector(".card")
 
 barIcon.addEventListener("click", () => {
   barIcon.classList.add("hide");
