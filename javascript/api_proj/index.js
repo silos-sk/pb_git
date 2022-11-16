7db3a25cfee207ccaf8f820be853b47227028742
silos-sk <97029496+silos-sk@users.noreply.github.com>
2022-11-16 15:51:48 +0000

    text style edits

--- javascript/api_proj/index.js
+++ javascript/api_proj/index.js
@@ -5,16 +5,16 @@ fetch(`https://imdb-api.com/en/API/ComingSoon/${apiKeyImdb}`).then((res) => {
   jsonRes
     .then((json) => {
       const movie = json.items;
-      console.log(movie);
-      console.log(movie[0].title);
-      console.log(movie[0].image);
+      // console.log(movie);
+      // console.log(movie[0].title);
+      // console.log(movie[0].image);
       for (let i = 0; i < 10; i++) {
         displayMovie(movie[i].title, movie[i].image, movie[i].releaseState);
       }
     })
     .catch(() => {
       //enter code here
-      console.log("error");
+      console.log("Error");
     });
 });
 
@@ -24,5 +24,4 @@ function displayMovie(item1, item2, item3) {
   section.appendChild(card);
   card.classList.add("card");
   card.innerHTML += `<h2>${item1}</h2><br><img src="${item2}" alt="${item2}"><p><h3>Release Date:</h3> ${item3}</p>`;
-
 }
